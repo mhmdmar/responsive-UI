cc0c5c97460f750215dc65a1b514f59c1c1783ca
yahyamuh <muhammad.yahya@microfocus.com>
2021-07-06 22:55:21 +0300

    more name changes

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const apiKey = "keyOw2loIpqsFxCW8";
 const apiUri = `https://api.airtable.com/v0/apppWiSUDpifZ2BL5`;
 
-const Api = axios.create({
+const api = axios.create({
     baseURL: apiUri
 });
 
-Api.defaults.headers.common["authorization"] = `Bearer ${apiKey}`;
-export default Api;
+api.defaults.headers.common["authorization"] = `Bearer ${apiKey}`;
+export default api;
